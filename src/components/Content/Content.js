3667b1e066923a3cbcc8d34abce17df2e633e5cd
unknown <haniffaiqx3@gmail.com>
2023-08-18 01:56:33 +0800

    add picture at content

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import profile from '../../assets/images/blank-profile-picture.jpg'
 const Content = () => {
     return (
-        <div>
-            <p className='text-gray-300'>THIS IS CONTENT !!</p>
+        <div className="text-white min-w-screen">
+            <nav className="py-4">
+                <div className="container mx-auto flex items-center align-center justify-center my-10 flex-col">
+                    <p className='mx-10 mb-4 text-center'>What are you looking for here? Are you searching about me?<br />
+                     Wow, I feel honored that you want to know about me. <br/>Come on, I'll help you get to know me better.</p>
+                    <img src={profile} alt="Profile" className="w-1/4 h-1/4 mr-2 rounded-full" />
+                </div>
+                <div className="container mx-auto flex items-center justify-between my-10">
+                    <p className=''>"Even a small change, it's a step forward."</p>
+                </div>
+            </nav>
         </div>
     );
 };
