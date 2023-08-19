0d1678275ab46b2c845a98daaa60afa6be398cbe
unknown <haniffaiqx3@gmail.com>
2023-08-19 14:41:30 +0800

    Testing automation 2

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -50,8 +50,8 @@ const Content = () => {
         <div className="text-white min-w-screen">
             <div className="py-4">
                 <div className="container mx-auto flex items-center align-center justify-center my-10 flex-col">
-                    <p className='mx-10 mb-4 text-center'>What are you looking for here? Are you searching about me?<br />
-                        Wow, I feel honored that you want to know about me. <br />Come on, I'll help you get to know me better.</p>
+                    {/* <p className='mx-10 mb-4 text-center'>What are you looking for here? Are you searching about me?<br />
+                        Wow, I feel honored that you want to know about me. <br />Come on, I'll help you get to know me better.</p> */}
                     <img src={profile} alt="Profile" className="w-1/4 h-1/4 mr-2 rounded-full" />
                 </div>
                 <div className="container mx-auto flex items-center justify-center my-10">
