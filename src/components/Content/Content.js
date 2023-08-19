480050d4873bb342035e01f0c367ccadef02dd26
unknown <haniffaiqx3@gmail.com>
2023-08-20 06:00:21 +0800

    small update

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -7,41 +7,46 @@ const experiences = [
         year: '2021',
         title: 'Frontend Developer',
         company: 'Company A',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
 
     {
         year: '2021',
         title: 'Frontend Developer',
         company: 'Company A',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
     },
 
     {
         year: '2021',
         title: 'Frontend Developer',
         company: 'Company A',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
     },
     {
         year: '2021',
         title: 'Frontend Developer',
         company: 'Company A',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
     },
 
     {
         year: '2021',
         title: 'Frontend Developer',
         company: 'Company A',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
     },
 
     {
         year: '2021',
         title: 'Frontend Developer',
         company: 'Company A',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
     },
     // ... add more experiences
 ];
@@ -49,15 +54,22 @@ const Content = () => {
     return (
         <div className="text-white min-w-screen">
             <div className="py-4">
-                <div className="container mx-auto flex items-center align-center justify-center my-10 flex-col">
-                    {/* <p className='mx-10 mb-4 text-center'>What are you looking for here? Are you searching about me?<br />
-                        Wow, I feel honored that you want to know about me. <br />Come on, I'll help you get to know me better.</p> */}
-                    <img src={profile} alt="Profile" className="w-1/4 h-1/4 mr-2 rounded-full" />
+                <div className="container mx-auto grid grid-cols-2 items-center my-10">
+                    <div className="col-span-1">
+                        <p className='mx-10 text-center align-center justify-center'>What are you looking for here? Are you searching about me?<br />
+                            Wow, I feel honored that you want to know about me. <br />Come on, I'll help you get to know me better.</p>
+                    </div>
+                    <div className="col-span-1 flex justify-center">
+                        <img src={profile} alt="Profile" className="w-1/2 h-1/2 mr-2 rounded-full" />
+                    </div>
                 </div>
                 <div className="container mx-auto flex items-center justify-center my-10">
-                    <p className=''>"Even a small change, it's a step forward."</p>
+                    <p className='text-2xl font-bold'> -- About Me -- </p>
                 </div>
-                <div className="container mx-auto flex items-center justify-between my-10">
+                <div className="container mx-auto flex items-center justify-center">
+                    <p className='text-2xl font-bold'>-- Working Experience -- </p>
+                </div>
+                <div className="container mx-auto flex items-center justify-between my-5">
                     <Timeline experiences={experiences} />
 
                 </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './style.css'
+import instagram_logo from '../../assets/images/icons/instagram.png'
+const SocialMediaLinks = () => {
+    const handleSocialMediaClick = (url) => {
+        window.open(url, '_blank'); // Membuka link di tab baru
+    };
+
+    return (
+        <div className="flex items-center space-x-4">
+            <button
+                onClick={() => handleSocialMediaClick('https://www.instagram.com/')}
+            >
+                <img src={instagram_logo} alt="Instagram" className="w-8 h-8" />
+            </button>
+            <button
+                onClick={() => handleSocialMediaClick('https://twitter.com/')}
+            >
+                <img src={instagram_logo} alt="Instagram" className="w-8 h-8" />
+            </button>
+            <button
+                onClick={() => handleSocialMediaClick('https://www.linkedin.com/in/')}
+            >
+                <img src={instagram_logo} alt="Instagram" className="w-8 h-8" />
+            </button>
+        </div>
+    );
+};
 const Footer = () => {
     return (
         <div className="text-white min-w-screen">
             <div className="container mx-auto flex items-center justify-center my-10">
                 <p className=''>"Even a small change, it's a step forward."</p>
             </div>
+            <div className="container mx-auto flex items-center justify-center my-10">
+            <SocialMediaLinks />
+            </div>
+
 
             <footer className="text-white py-4">
                 <p className="text-center copyright">&copy; 2023 lama2dingin.id</p>
